92f5a62d69a58954cd83b1aa7496d5f672732aa3
Chase McCoy <chasem000@gmail.com>
2021-02-13 14:22:10 -0600

    add nypl api

---
+++ api/nypl.js
@@ -0,0 +1,33 @@
+const fetch = require('node-fetch')
+
+const API_ENDPOINT = query => `http://api.repo.nypl.org/api/v1/items/search?q=${query}&publicDomainOnly=true&per_page=100`
+
+const IMAGE_URL = (id) => `http://images.nypl.org/index.php?id=${id}&t=w`
+
+exports.handler = async (event, context) => {
+  const query = event.queryStringParameters.query
+
+  try {
+    const response = await fetch(API_ENDPOINT(query), {headers: {
+      'Authorization': `Token token="${process.env.NYPL_TOKEN}"`
+    }})
+    const json = await response.json()
+
+    const data = json.nyplAPI.response.result.map(item => ({
+      title: item.title,
+      image: IMAGE_URL(item.imageID)
+    }))
+
+    // data.forEach(item => console.log(item.image))
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }
+  } catch (error) {
+    return {
+      statusCode: 422,
+      body: String(error)
+    }
+  }
+}
