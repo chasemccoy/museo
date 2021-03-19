695e277bfa85befb7cfe81f765514be6b9739828
Chase McCoy <chasem000@gmail.com>
2021-03-19 16:47:49 -0500

    Revert "remove harvard for now"
    This reverts commit 7f1836fedfff7a8e63b071fe4d477629d797e016.

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,10 +38,10 @@ export default function Home() {
     ['rijks', searchTerm],
     fetchData
   )
-  // const { data: harvard, isLoading: loadingHarvard } = useQuery(
-  //   ['harvard', searchTerm],
-  //   fetchData
-  // )
+  const { data: harvard, isLoading: loadingHarvard } = useQuery(
+    ['harvard', searchTerm],
+    fetchData
+  )
   const { data: artsmia, isLoading: loadingArtsmia } = useQuery(
     ['artsmia', searchTerm],
     fetchData
@@ -49,11 +49,12 @@ export default function Home() {
 
   let data = null
   const loading =
-    loadingAIChicago || loadingNYPL || loadingRijks || loadingArtsmia
+    loadingAIChicago || loadingNYPL || loadingRijks || loadingHarvard || loadingArtsmia
 
-  if (aiChicago && nypl && rijks && artsmia) {
+  if (aiChicago && nypl && rijks && harvard && artsmia) {
     data = interleave(aiChicago, nypl)
     data = interleave(data, rijks)
+    data = interleave(data, harvard)
     data = interleave(data, artsmia)
   }
 
@@ -83,7 +84,9 @@ export default function Home() {
             <a href='https://www.artic.edu/archival-collections/explore-the-collection'>
               Art Institute of Chicago
             </a>
-            , the <a href='https://www.rijksmuseum.nl/nl'>Rijksmuseum</a>, the{' '}, the <a href="https://artsmia.org">Minneapolis Institute of Art</a> and
+            , the <a href='https://www.rijksmuseum.nl/nl'>Rijksmuseum</a>, the{' '}
+            <a href='https://harvardartmuseums.org'>Harvard Art Museums</a>,
+            the <a href="https://artsmia.org">Minneapolis Institute of Art</a> and
             the{' '}
             <a href='https://digitalcollections.nypl.org'>
               New York Public Library Digital Collection
